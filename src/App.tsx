431c6248a6e6a6b06f06db03accbedfea1336a14
tsteelematc <tsteele@madisoncollege.edu>
2023-02-21 13:34:25 -0600

    Added dummy Home component...
    With some bootstrap styled items. Specifically Button and Card.
    
    And it worked ! ! !

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Home = () => (
+  <>
+    <h1>
+      TCA Bar React TS Bootstrap
+    </h1>
+    <h2>
+      Companion App
+    </h2>
+    <Button 
+      variant="outline-primary"
+    >
+      Play Bar
+    </Button>
+    <Card>
+      <Card.Header>
+        Leaderboard
+      </Card.Header>
+      <Card.Body>
+        Play a game to see your leaderboard...
+      </Card.Body>
+    </Card>
+  </>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
