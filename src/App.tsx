4df5b61578371c93aaeeafaffff402a11d2fa95a
tsteelematc <tsteele@madisoncollege.edu>
2023-02-23 13:03:54 -0600

    screens/initial-navigation kind of working...
    Still need buttons to navigate ! ! !
    
    We'll do that next ! ! !

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { Home } from './Home';
+import { Setup } from './Setup';
+import { Play } from './Play';
+
+import {
+  HashRouter
+  , Routes 
+  , Route
+} from 'react-router-dom';
 
 function App() {
   return (
@@ -14,7 +23,13 @@ function App() {
         Companion App
       </h2>
       <hr />
-      <Home />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/setup" element={<Setup />} />
+          <Route path="/play" element={<Play />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
